46b6c76cd204640c94a17f69cf61d443f9957264
朱亮 <teamide@163.com>
2023-01-14 10:49:07 +0800

    跟随electron-react-boilerplate升级

--- src/renderer/preload.d.ts
+++ src/renderer/preload.d.ts
@@ -1,18 +1,9 @@
-import { Channels } from 'main/preload';
+import { ElectronHandler } from 'main/preload';
 
 declare global {
   interface Window {
-    electron: {
-      ipcRenderer: {
-        sendMessage(channel: Channels, args: unknown[]): void;
-        on(
-          channel: Channels,
-          func: (...args: unknown[]) => void
-        ): (() => void) | undefined;
-        once(channel: Channels, func: (...args: unknown[]) => void): void;
-      };
-    };
+    electron: ElectronHandler;
   }
 }
 
-export {};
+export { };
